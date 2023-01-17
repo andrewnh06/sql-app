130d022526921c5dab3cb19205d0522e962c306b
nxmespxce <naturalsteak23@gmail.com>
2023-01-17 10:39:11 -0700

    GUI initial

--- sql-app/gui.py
+++ sql-app/gui.py
@@ -2,6 +2,18 @@ import customtkinter
 import tkinter
 
 class App(customtkinter.CTk):
+    def register_window(self):
+        new_window = customtkinter.CTkToplevel(self)
+        new_window.title("Register")
+        new_window.geometry("400x500")
+
+        main_frame = customtkinter.CTkFrame(master=new_window)
+        main_frame.pack(padx=60, pady=20, fill="both", expand=True)
+
+        pad_canvas = customtkinter.CTkCanvas(master=main_frame, height=40, bg='#2b2b2b')
+        pad_canvas.configure(highlightthickness=0)
+        pad_canvas.pack(pady=10)
+
     def __init__(self):
         super().__init__()
 
@@ -24,6 +36,9 @@ class App(customtkinter.CTk):
         user_entry = customtkinter.CTkEntry(master=main_frame, placeholder_text="Type here...")
         user_entry.pack()
 
+        def print_entry():
+            print(user_entry.get())
+
         pad_canvas1 = customtkinter.CTkCanvas(master=main_frame, height=40, bg='#2b2b2b')
         pad_canvas1.configure(highlightthickness=0)
         pad_canvas1.pack(pady=10)
@@ -34,8 +49,8 @@ class App(customtkinter.CTk):
         pass_entry = customtkinter.CTkEntry(master=main_frame, placeholder_text="Type here...")
         pass_entry.pack()
 
-        button = customtkinter.CTkButton(master=main_frame, text="Login")
-        button.place(relx = 0.5, rely = 0.9, anchor=tkinter.CENTER)
+        button = customtkinter.CTkButton(master=main_frame, text="Login", command=print_entry)
+        button.place(relx = 0.5, rely = 0.8, anchor=tkinter.CENTER)
 
-app = App()
-app.mainloop()
+        register_button = customtkinter.CTkButton(master=main_frame, text="Don't have an account?", command=self.register_window)
+        register_button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
